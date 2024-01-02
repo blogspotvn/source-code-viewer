9f3c4c49b72e7a47d99a219d31be3d5c37052151
blogspotvn <155406000+blogspotvn@users.noreply.github.com>
2024-01-02 10:25:37 +0700

    Update view-source.js

--- view-source.js
+++ view-source.js
@@ -6,15 +6,14 @@ async function fetchSourceCode() {
 
     const blogIdRegex = /'blogId': '(\d+)'|targetBlogID=(\d+)/;
     const blogIdMatch = text.match(blogIdRegex);
-    let blogId = blogIdMatch ? (blogIdMatch[1] ? blogIdMatch[1] : blogIdMatch[2]) : "Không tìm thấy BlogId";
+    let blogId = blogIdMatch ? (blogIdMatch[1] ? blogIdMatch[1] : blogIdMatch[2]) : "Không tìm thấy Blog Id";
 
     const titleRegex = /<title>(.*?)<\/title>/;
     const titleMatch = text.match(titleRegex);
-    const title = titleMatch ? titleMatch[1] : "Không tìm thấy Title";
+    const title = titleMatch ? titleMatch[1] : "Không tìm thấy Website";
 
-    document.getElementById('sourceCode').textContent = `Blog Id: ${blogId}\nWebstie: ${title}`;
+    document.getElementById('sourceCode').textContent = `BlogId: ${blogId}\nWebsite: ${title}`;
   } catch (error) {
     console.error('Có lỗi xảy ra:', error);
   }
 }
-
